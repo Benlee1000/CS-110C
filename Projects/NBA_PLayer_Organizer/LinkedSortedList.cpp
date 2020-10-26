ddff5f73032f9db867a0b5b1be5beac5319a5712
Benlee1000 <60490332+Benlee1000@users.noreply.github.com>
2020-10-25 20:53:59 -0700

    Worked on project

--- Projects/NBA_PLayer_Organizer/LinkedSortedList.cpp
+++ Projects/NBA_PLayer_Organizer/LinkedSortedList.cpp
@@ -33,15 +33,18 @@ Node<Player>* LinkedSortedList<Player>::copyChain(const Node<Player>* origChainP
   return copiedChainPtr;
 } // end copyChain 
 
-LinkedSortedList<Player>::LinkedSortedList() {
-}
+LinkedSortedList<Player>::LinkedSortedList() : headPtr(nullptr), itemCount(0) {
+} // end default constructor 
+
+LinkedSortedList<Player>::~LinkedList() {
+  clear();
+} // end destructor 
 
 LinkedSortedList<Player>::LinkedSortedList(const LinkedSortedList<Player>& aList) {
   headPtr = copyChain(aList.headPtr); 
-}
+} // end copy constructor
 
-void LinkedSortedList<Player>::insertSorted(const Player& newEntry)
-{
+void LinkedSortedList<Player>::insertSorted(const Player& newEntry) {
   Node<Player>* newNodePtr = new Node<Player>(newEntry);
   Node<Player>* prevPtr = getNodeBefore(newEntry);
 
@@ -57,4 +60,48 @@ void LinkedSortedList<Player>::insertSorted(const Player& newEntry)
 
   itemCount++;
 } // end insertSorted 
+bool LinkedStack<Player>::isEmpty() const {
+  return headPtr == nullptr;
+} // end isEmpty 
+
+int LinkedStack<Player>::getLength() const {
+  return itemcount;
+}
+
+bool LinkedSortedList<PLayer>::remove(int position) {
+  bool ableToRemove = (position >= 1) &&(position <= itemCount);
+  if (ableToRemove) {
+    Node<Player>* curPtr = nullptr;
+    if (position == 1) {
+    // Remove the first node in the chain
+    curPtr = headPtr; // Save pointer to node
+    headPtr = headPtr->getNext();
+    }
+    else {
+    // Find node that is before the one to delete
+    Node<Player>* prevPtr = getNodeAt(position - 1);
+    // Point to node to delete
+    curPtr = prevPtr->getNext();
+    // Disconnect indicated node from chain by connecting the
+    // prior node with the one after
+    prevPtr->setNext(curPtr->getNext());
+    } // end else
+
+    // Return node to system
+    curPtr->setNext( nullptr); 
+
+    delete curPtr;
+    curPtr = nullptr;
+
+    itemCount–-; // Decrease count of entries
+    } // end if
+
+  return ableToRemove;
+} // end remove 
+
+void LinkedSortedList<Player>::clear() {
+  while (!isEmpty()){
+    remove(1);
+  }  
+} // end clear 
 
